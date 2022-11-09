95b63b496b4813f6b6ebac8d944c20a836597147
dayong <wangdayong228@163.com>
2022-11-09 18:28:15 +0800

    add gorm tag for models.WechatRefundDetail

--- models/refunds.go
+++ models/refunds.go
@@ -8,27 +8,27 @@ import (
 
 type WechatRefundDetail struct {
 	BaseModel
-	RefundId *string `json:"refund_id"`
+	RefundId *string `gorm:"type:varchar(32)" json:"refund_id"`
 	// 商户系统内部的退款单号，商户系统内部唯一，只能是数字、大小写字母_-|*@ ，同一退款单号多次请求只退一笔。
-	RefundNo string `json:"out_refund_no" gorm:"uniqueIndex"`
+	RefundNo string `gorm:"type:varchar(32)" json:"out_refund_no" `
 	// 微信支付交易订单号
-	TransactionId *string `json:"transaction_id"`
+	TransactionId *string `gorm:"type:varchar(32)" json:"transaction_id"`
 	// 原支付交易对应的商户订单号
-	TradeNo string `json:"out_trade_no"`
+	TradeNo string `gorm:"type:varchar(32);uniqueIndex" json:"out_trade_no"`
 	// 枚举值： - ORIGINAL—原路退款 - BALANCE—退回到余额 - OTHER_BALANCE—原账户异常退到其他余额账户 - OTHER_BANKCARD—原银行卡异常退到其他银行卡 * `ORIGINAL` - 原路退款 * `BALANCE` - 退回到余额 * `OTHER_BALANCE` - 原账户异常退到其他余额账户 * `OTHER_BANKCARD` - 原银行卡异常退到其他银行卡
-	Channel *string `json:"channel"`
+	Channel *string `gorm:"type:varchar(32)" json:"channel"`
 	// 取当前退款单的退款入账方，有以下几种情况： 1）退回银行卡：{银行名称}{卡类型}{卡尾号} 2）退回支付用户零钱:支付用户零钱 3）退还商户:商户基本账户商户结算银行账户 4）退回支付用户零钱通:支付用户零钱通
-	UserReceivedAccount *string `json:"user_received_account"`
+	UserReceivedAccount *string `gorm:"type:varchar(32)" json:"user_received_account"`
 	// 退款成功时间，退款状态status为SUCCESS（退款成功）时，返回该字段。遵循rfc3339标准格式，格式为YYYY-MM-DDTHH:mm:ss+TIMEZONE，YYYY-MM-DD表示年月日，T出现在字符串中，表示time元素的开头，HH:mm:ss表示时分秒，TIMEZONE表示时区（+08:00表示东八区时间，领先UTC 8小时，即北京时间）。例如：2015-05-20T13:29:35+08:00表示，北京时间2015年5月20日13点29分35秒。
 	SuccessTime *time.Time `json:"success_time,omitempty"`
 	// 退款受理时间，遵循rfc3339标准格式，格式为YYYY-MM-DDTHH:mm:ss+TIMEZONE，YYYY-MM-DD表示年月日，T出现在字符串中，表示time元素的开头，HH:mm:ss表示时分秒，TIMEZONE表示时区（+08:00表示东八区时间，领先UTC 8小时，即北京时间）。例如：2015-05-20T13:29:35+08:00表示，北京时间2015年5月20日13点29分35秒。
 	CreateTime *time.Time `json:"create_time"`
 	// 退款到银行发现用户的卡作废或者冻结了，导致原路退款银行卡失败，可前往商户平台（pay.weixin.qq.com）-交易中心，手动处理此笔退款。 枚举值： - SUCCESS—退款成功 - CLOSED—退款关闭 - PROCESSING—退款处理中 - ABNORMAL—退款异常 * `SUCCESS` - 退款成功 * `CLOSED` - 退款关闭 * `PROCESSING` - 退款处理中 * `ABNORMAL` - 退款异常
-	Status *string `json:"status"`
+	Status *string `gorm:"type:varchar(32)" json:"status"`
 	// 退款通知使用字段，需要我们的程序保持跟status始终同步
-	RefundStatus *string `json:"refund_status"`
+	RefundStatus *string `gorm:"-" json:"refund_status"`
 	// 退款所使用资金对应的资金账户类型 枚举值： - UNSETTLED : 未结算资金 - AVAILABLE : 可用余额 - UNAVAILABLE : 不可用余额 - OPERATION : 运营户 - BASIC : 基本账户（含可用余额和不可用余额） * `UNSETTLED` - 未结算资金 * `AVAILABLE` - 可用余额 * `UNAVAILABLE` - 不可用余额 * `OPERATION` - 运营户 * `BASIC` - 基本账户（含可用余额和不可用余额）
-	FundsAccount *string `json:"funds_account,omitempty"`
+	FundsAccount *string `gorm:"type:varchar(32)" json:"funds_account,omitempty"`
 	// 金额详细信息
 	Amount *int64 `json:"amount"`
 }
