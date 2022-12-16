7c6bfdced9a0e0c16ffbf217143d96033708190a
dayong <wangdayong228@163.com>
2022-12-16 18:27:26 +0800

    enlarge emoji pattern range

--- utils/strings.go
+++ utils/strings.go
@@ -3,7 +3,7 @@ package utils
 import "regexp"
 
 func ReplaceEmoji(source string, replacement string) string {
-	reg := regexp.MustCompile(`[\x{00A1}-\x{00AF}]|[\x{1F251}-\x{1F6FF}]|[\x{2000}-\x{3000}]`)
+	reg := regexp.MustCompile(`[\x{00A1}-\x{00AF}]|[\x{1F000}-\x{1FFFF}]|[\x{2000}-\x{3000}]`)
 	converted := reg.ReplaceAll([]byte(source), []byte(replacement))
 	return string(converted)
 }
--- utils/strings_test.go
+++ utils/strings_test.go
@@ -12,10 +12,15 @@ func TestReplaceEmoji(t *testing.T) {
 	result := ReplaceEmoji(source, "[e]")
 	fmt.Println(result)
 	assert.Equal(t, result, "[e][e][e][e][e][e][e][e][e][e][e][e][e][e][e][e]")
+
+	source = "122223334👌👌🤝😢😂🤣123456789"
+	result = ReplaceEmoji(source, "[e]")
+	fmt.Println(result)
+	assert.Equal(t, result, "122223334[e][e][e][e][e][e]123456789")
 }
 
 func TestEmojiValues(t *testing.T) {
-	source := "⌛️😁🙏✂➰🚀🛀Ⓜ🉑©🗿😀😶🚁🛅🌍🕧"
+	source := "🤝🤣⌛️😁🙏✂➰🚀🛀Ⓜ🉑©🗿😀😶🚁🛅🌍🕧"
 	for _, v := range source {
 		fmt.Printf("%x ", v)
 		fmt.Printf("%c ", v)
